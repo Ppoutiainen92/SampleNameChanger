c144dbd363836c49560ade383383b2d121fef231
Petri Poutiainen <Ppoutiainen92@gmail.com>
2020-02-25 17:17:10 +0200

    Select files added

--- WPFUI/ViewModels/ShellViewModel.cs
+++ WPFUI/ViewModels/ShellViewModel.cs
@@ -11,12 +11,37 @@ namespace WPFUI.ViewModels
     public class ShellViewModel : Conductor<object>
     {
         private string _blockText = "jotain";
+        public BindableCollection<string> Items { get; set; }
 
-        
+        public ShellViewModel()
+        {
+            Items = new BindableCollection<string>();
+
+        }
+
         public void SelectFiles()
         {
-            _blockText = "Jotain muuta";
-            NotifyOfPropertyChange(() => Block);
+            //_blockText = "Jotain muuta";
+            //Items.Add(_blockText);
+            //NotifyOfPropertyChange(() => Block);
+
+
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.Multiselect = true;
+            dlg.DefaultExt = ".rtf";
+            dlg.Filter = "TXT Files(*.txt)|*.txt|RTF files(*.rtf)|*.rtf";
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if(result== true)
+            {
+                foreach(String file in dlg.FileNames)
+                {
+                Items.Add(dlg.FileName);
+
+                }
+
+            }
 
         }
 
