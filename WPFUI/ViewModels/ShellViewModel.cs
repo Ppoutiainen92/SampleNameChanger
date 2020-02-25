09e869ccdd44d9e053665edd2bb8d541abdc2496
Petri Poutiainen <Ppoutiainen92@gmail.com>
2020-02-25 16:50:45 +0200

    Init

--- WPFUI/ViewModels/ShellViewModel.cs
+++ WPFUI/ViewModels/ShellViewModel.cs
@@ -4,11 +4,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace WPFUI.ViewModels
 {
-    public class ShellViewModel
+    public class ShellViewModel : Conductor<object>
     {
+        private string _blockText = "jotain";
 
+        
+        public void SelectFiles()
+        {
+            _blockText = "Jotain muuta";
+            NotifyOfPropertyChange(() => Block);
+
+        }
+
+        public string Block
+        {
+            get { return _blockText; }
+
+
+
+        }
     }
 }
