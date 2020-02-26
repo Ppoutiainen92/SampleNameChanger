557e25ddd86e6bd5c95b3cdfcc5739acf56db190
Petri Poutiainen <Ppoutiainen92@gmail.com>
2020-02-26 14:49:39 +0200

    Checkbox backend started

--- WPFUI/ViewModels/ShellViewModel.cs
+++ WPFUI/ViewModels/ShellViewModel.cs
@@ -12,14 +12,17 @@ namespace WPFUI.ViewModels
     public class ShellViewModel : Conductor<object>
     {
         private string _blockText = "jotain";
+        private bool _checkBoxppp;
+
         public BindableCollection<string> FilePaths { get; set; }
+        public BindableCollection<string> CheckBoxStates { get; set; }
         List<FileInfo> listOfSelectedFiles;
         int _roundRobinValue = 0;
 
         public ShellViewModel()
         {
             FilePaths = new BindableCollection<string>();
-
+            CheckBoxStates = new BindableCollection<string>();
         }
         
         public void SelectFiles()
@@ -49,5 +52,31 @@ namespace WPFUI.ViewModels
      
 
         }
+
+
+        public bool CheckBoxppp
+        {
+            get { return _checkBoxppp; }
+            set {
+                _checkBoxppp = value;
+                if (_checkBoxppp)
+                {
+
+                    CheckBoxStates.Add($"CheckBoxppp is {_checkBoxppp}");
+                    NotifyOfPropertyChange(() => CheckBoxStates);
+                }
+                else
+                {
+                    if(CheckBoxStates.Contains("CheckBoxppp is True"))
+                    {
+                        CheckBoxStates.Remove("CheckBoxppp is True");
+                        NotifyOfPropertyChange(() => CheckBoxStates);
+                    }
+
+                }
+                
+            }
+        }
+
     }
 }
