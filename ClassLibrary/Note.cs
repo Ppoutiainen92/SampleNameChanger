6098674a2fe5765ab03f89308e8a041a6ab0bb8e
Petri Poutiainen <Ppoutiainen92@gmail.com>
2020-03-04 07:44:16 +0200

    Note class added

---
+++ ClassLibrary/Note.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Note
+    {
+        public string noteName;
+        public int octave;
+
+        public Note(string NoteName, int Octave)
+        {
+            NoteName = noteName;
+            Octave = octave;
+
+        }
+
+
+
+        public override string ToString()
+        {
+            return noteName +", Oct:" +octave;
+        }
+
+    }
+}
